4de16ff0d4c7a3c67732f6a1b80ac8f1ede60625
Zac Dong <19960825dongsiyao@gmail.com>
2019-11-05 08:50:15 +0800

    Springcloud_demo

--- itcast_springcloud/consumer_demo/src/main/java/cn/itcast/consumer/controller/ConsumerController.java
+++ itcast_springcloud/consumer_demo/src/main/java/cn/itcast/consumer/controller/ConsumerController.java
@@ -2,10 +2,15 @@ package cn.itcast.consumer.controller;
 
 import cn.itcast.consumer.pojo.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 /**
  * @Author Zac Dong
@@ -19,9 +24,21 @@ public class ConsumerController {
     @Autowired
     private RestTemplate restTemplate;
 
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
     @GetMapping("/{id}")
-    public User queryById(Long id) {
-        String url = "http://localhost:9091/user/11";
+    public User queryById(@PathVariable Long id) {
+/*
+        String url = "http://localhost:9091/user/" + id;
+
+        //获取Eureka中注册的user-service的实例
+        List<ServiceInstance> serviceInstances = discoveryClient.getInstances("user-service");
+        ServiceInstance serviceInstance = serviceInstances.get(0);
+        url = "http://" + serviceInstance.getHost() + ":" + serviceInstance.getPort() + "/user/" + id;
+*/
+
+        String url = "http://user-service/user/" + id;
         return restTemplate.getForObject(url, User.class);
     }
 
